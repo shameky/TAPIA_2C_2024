7209bb789fdac4b806ba138fe90fe0f39567ff1d
shameky <110622254+shameky@users.noreply.github.com>
2024-11-11 15:39:52 -0300

    Recuperatorio
    creacion de proyecto

---
+++ firmware/projects/parcial_recuperatorio/main/x_template.c
@@ -0,0 +1,39 @@
+/*! @mainpage Parcial - Recuperatorio
+ *
+ * @section genDesc General Description
+ *
+ * This section describes how the program works.
+ *
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 11/11/2024 | Document creation		                         |
+ *
+ * @author Jorge Ignacio Tapia (jorge.tapia@ingeneiria.uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void)
+{
+
+}
+/*==================[end of file]============================================*/
